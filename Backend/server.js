0466d1641e5203d9710955834806c0d16f24add8
Sukhdev Miyatra <sukhdevmiyatra2@gmail.com>
2023-12-07 19:09:38 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { TextServiceClient } = require('@google-ai/generativelanguage').v1beta2;
+const { GoogleAuth } = require('google-auth-library');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+const MODEL_NAME = 'models/text-bison-001';
+const API_KEY = process.env.API_KEY;
+
+if (!API_KEY) {
+  console.error('API_KEY is missing. Please provide a valid API key.');
+  process.exit(1);
+}
+
+app.post('/generate-sql', async (req, res) => {
+  try {
+    const userInput = req.body.userInput;
+
+    const client = new TextServiceClient({
+      authClient: new GoogleAuth().fromAPIKey(API_KEY),
+    });
+
+    const prompt = `
+      You are a SQL expert. You have to recreate the SQL query for text provided to you by user. '''don't include any other explanation.'''
+     
+      User Has said:
+      ${userInput}
+
+      '''Begin Generating Only SQL Code:'''
+    `;
+
+    const result = await client.generateText({
+      model: MODEL_NAME,
+      prompt: {
+        text: prompt,
+      },
+    });
+
+    const generatedText = result[0]?.candidates[0]?.output;
+
+    if (generatedText) {
+      res.json({ generatedText });
+    } else {
+      res.status(500).json({ error: 'Failed to generate SQL code.' });
+    }
+  } catch (error) {
+    
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+ 
+});
---
+++ Fronted/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [text, setText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const getResponse = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/generate-sql`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userInput: text }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to get response from the server');
+      }
+
+      const data = await response.json();
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          author: 'User',
+          content: [text], // User message as an array
+        },
+        {
+          author: 'Bot',
+          content: data.generatedText.split('\n'), // Bot response as an array of lines
+        },
+      ]);
+      setText('');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-feed">
+        {messages?.map((message, index) => (
+          <div key={index} className={`message ${message.author.toLowerCase()}`}>
+            <strong>{message.author}: </strong>
+            {message.content.map((line, lineIndex) => (
+              <p key={lineIndex}>{line}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <textarea
+          placeholder="Type your message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          rows={4} // Adjust the number of rows as needed
+        />
+        <button onClick={getResponse}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
