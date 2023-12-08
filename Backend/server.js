a91454f6ca8d9c2a90aab5701ba55a3bf861b7e8
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-12-08 10:45:05 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.post('/generate-sql', async (req, res) => {
     });
 
     const prompt = `
-      You are a SQL expert. You have to recreate the SQL query for text provided to you by user. '''don't include any other explanation.'''
+      You are a SQL expert. You have to recreate the SQL query for text provided to you by user. '''Don't include any other explanation. and reply with please send SQL query if user has sent unrelated message, input or query'''
      
       User Has said:
       ${userInput}
