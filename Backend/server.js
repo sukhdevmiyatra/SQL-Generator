03314ed0d3555a77a8721f4c2e183dc379f3cf4b
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-12-08 10:25:06 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,44 +10,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json())
 app.use(morgan('dev'));
-
-
-var whitelist = ['https://sql-generator-gamma.vercel.app/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-app.use(cors(corsOptions));
+app.use(cors);
 
 const MODEL_NAME = 'models/text-bison-001';
 const API_KEY = process.env.API_KEY;
